182579a005a90b73725d8df89e5cbed958c2a797
PhamMinhKha0710 <phamminhkha19091710@gmail.com>
2026-02-14 14:22:46 +0700

    fix(admin): remove unused Navigate import and replace any with unknown in UsersPage

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Toaster } from 'sonner'
 import { store } from '@/app/store'
